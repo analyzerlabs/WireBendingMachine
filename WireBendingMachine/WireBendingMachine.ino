c157139c8648d747faad3aa4c6f82c666c468c31
analyzerlabs <miguelquispecastro@uni.pe>
2019-11-20 17:35:41 -0500

    update

---
+++ WireBendingMachine/WireBendingMachine.ino
@@ -0,0 +1,83 @@
+#include <UTFTGLUE.h>              //use GLUE class and constructor
+UTFTGLUE myGLCD(0,A2,A1,A3,A4,A0); //all dummy args
+//UTFT myGLCD(ITDB32S,38,39,40,41);
+
+void setup()
+{
+  randomSeed(analogRead(0)); 
+// Setup the LCD
+  myGLCD.InitLCD();
+  myGLCD.setFont(SmallFont);
+}
+
+void loop()
+{
+  int buf[318];
+  int x, x2;
+  int y, y2;
+  int r;
+
+// Clear the screen and draw the frame
+ 
+  myGLCD.setColor(0,0,0);
+  myGLCD.fillRect(1,15,318,224);
+  TouchInit();
+// Draw some filled, rounded rectangles
+  for (int i=1; i<6; i++)
+  {
+    switch (i)
+    {
+      case 1:
+        myGLCD.setColor(255,0,255);
+        break;
+      case 2:
+        myGLCD.setColor(255,0,0);
+        break;
+      case 3:
+        myGLCD.setColor(0,255,0);
+        break;
+      case 4:
+        myGLCD.setColor(0,0,255);
+        break;
+      case 5:
+        myGLCD.setColor(255,255,0);
+        break;
+    }
+    myGLCD.fillRoundRect(190-(i*20), 30+(i*20), 250-(i*20), 90+(i*20));
+  }
+  
+  delay(2000);
+  
+  myGLCD.setColor(0,0,0);
+  myGLCD.fillRect(1,15,318,224);
+
+// Draw some filled circles
+  for (int i=1; i<6; i++)
+  {
+    switch (i)
+    {
+      case 1:
+        myGLCD.setColor(255,0,255);
+        break;
+      case 2:
+        myGLCD.setColor(255,0,0);
+        break;
+      case 3:
+        myGLCD.setColor(0,255,0);
+        break;
+      case 4:
+        myGLCD.setColor(0,0,255);
+        break;
+      case 5:
+        myGLCD.setColor(255,255,0);
+        break;
+    }
+    myGLCD.fillCircle(100+(i*20),60+(i*20), 30);
+  }
+  
+  delay(2000);
+  
+  myGLCD.setColor(0,0,0);
+  myGLCD.fillRect(1,15,318,224);
+
+}
